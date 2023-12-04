7978391b9e916e6409c7fdd10ea6416b4293fa19
davidbmjt1 <149785156+davidbmjt1@users.noreply.github.com>
2023-12-04 11:41:38 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,202 +1,77 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-
-//import static java.lang.StringTemplate.STR;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Tienda informática");
 
         Scanner input = new Scanner(System.in);
-        ArrayList<Ventas> ventas = new ArrayList<>();
+        Tienda tienda = new Tienda();
 
-        boolean continuar_programa = true;
 
+        Vendedor vendedor1 = new Vendedor("Juan");
+        Vendedor vendedor2 = new Vendedor("Maria");
+
+        Producto producto1 = new Producto("Laptop", 1200.0, 800.0, vendedor1);
+        Producto producto2 = new Producto("Impresora", 300.0, 150.0, vendedor2);
+        Producto producto3 = new Producto("Monitor", 500.0, 300.0, vendedor1);
+
+        vendedor1.venderProducto(producto1);
+        vendedor2.venderProducto(producto2);
+        vendedor1.venderProducto(producto3);
+
+
+        tienda.agregarProducto(producto1);
+        tienda.agregarProducto(producto2);
+        tienda.agregarProducto(producto3);
+
+
+
+        boolean gestion = true;
         do {
-
             String menu = """
-                    1 - Introducir venta
-                    2 - Mostrar todas las ventas
-                    3 - Mostrar beneficio actual
-                    4 - Producto con mayor beneficio
-                         """;
+                    1 - Comprar producto
+                    2 - Mejor vendedor
+                    3 - Mejor producto
+                    """;
 
             System.out.println(menu);
-
-            int opcion = input.nextInt();
+            String opcion = input.next();
 
             switch (opcion) {
-                case 1:
-                    crearVenta(input, ventas);
+                case "1":
+                    comprarProducto(input, tienda);
                     break;
-
-
-                case 2:
-                    for (int i = 0; i < ventas.size(); i++) {
-                        System.out.println(ventas.get(i));
-                    }
+                case "2":
+                    Vendedor mejorVendedor = tienda.obtenerMejorVendedor();
+                    System.out.println("Mejor Vendedor: " + mejorVendedor.getNombre());
                     break;
-
-                case 3:
-                    float total = 0;
-                    for (int i = 0; i < ventas.size(); i++) {
-                        total += ventas.get(i).getBeneficio_venta();
-                    }
-                    System.out.println("Beneficio actual:" + total + "€");
-                    break;
-
-
-                default:
-                    System.out.println("Opción no reconocida");
-                    continuar_programa = false;
-
-            }
-
-
-            //Ventas venta1 = new Ventas("portátil", 2, 500, 300, "Bill Gates");
-            //Ventas venta2 = new Ventas("monitor", 5, 250, 180, "Elon Musk");
-
-
-            //////ventas.add(venta1);
-            //////ventas.add(venta2);
-
-
-            /*
-            for (int i = 0; i < ventas.size(); i++) {
-                //System.out.println();
-                System.out.println(ventas.get(i)); // + nuestras_ventas.get(i));
-                System.out.println("El beneficio ha sido ");
-
-            }
-*/
-
-            //System.out.println(venta1);
-            //////System.out.println("Beneficio venta #1: " + beneficio_venta1);
-
-
-
-/*
-            System.out.println("Por favor, indique su nombre (vendedor):");
-            String nombreVendedor = input.next();
-
-            switch (nombreVendedor){
-                case "Juan":
-                    actuacionVendedor("Juan", input);
-                    break;
-                case "Bill":
-                    actuacionVendedor("Bill", input);
-                    break;
-                case "Elon":
-                    actuacionVendedor("Elon", input);
+                case "3":
+                    Producto productoMasRentable = tienda.obtenerProductoMasRentable();
+                    System.out.println("Producto Más Rentable: " + productoMasRentable.getNombre());
                     break;
                 default:
-                    System.out.println("Error en la identificación");
+                    gestion = false;
+
             }
-*/
+        } while (gestion);
 
 
-        } while (continuar_programa);
 
     }
 
+    private static void comprarProducto(Scanner input, Tienda tienda) {
+        System.out.println("¿Nombre del producto?");
+        String nombreProducto = input.next();
+        System.out.println("¿Precio de venta?");
+        double precioVenta = input.nextDouble();
+        System.out.println("¿Precio de coste?");
+        double precioCoste = input.nextDouble();
+        System.out.println("¿Nombre del vendedor?");
+        String nombreVendedor = input.next();
 
-        public static void crearVenta (Scanner input, ArrayList ventas) {
-            System.out.println("Nombre del producto: ");
-            String nombreProducto = input.next();
-
-            System.out.println("Cantidad de unidades: ");
-            int unidades = input.nextInt();
-
-            System.out.println("Precio venta: ");
-            float precioVenta = input.nextFloat();
-
-            System.out.println("Precio coste: ");
-            float precioCoste = input.nextFloat();
-
-            System.out.println("Vendedor: ");
-            String vendedor = input.next();
-
-            /*
-            for (int i = 0; i < ventas.size() ; i++) {
-                if (ventas.get(i).getNombre_producto() == nombreProducto) {
-                    subtotalBeneficio = ventas.get(i).get
-                }
-            }
-            */
-
-
-            Ventas venta = new Ventas(nombreProducto, unidades, precioVenta, precioCoste, vendedor);
-            ventas.add(venta);
-
-
-
-        }
-
-        /*
-        public static String obtenerBenneficios (ArrayList ventas) {
-            float beneficio = 0;
-            for (int i = 0; i < ventas.size(); i++) {
-                beneficio +=  ventas.get(i).getBeneficio_venta();
-            }
-            return
-        }
-        */
-
-
-/*
-
-    private static void actuacionVendedor(String vendedor, Scanner input) {
-
-        String menu = STR."""
-        Bienvenido/a \{vendedor}
-               Operaciones disponibles:
-1 - Crear venta productos     2 - Obtener beneficios o pérdidas actuales
-3 - Vendedor líder            4 - Producto líder
-               Cualquier otra tecla para finalizar
-               """;
-
-        short eleccion;
-
-        do {
-            System.out.println(menu);
-            eleccion = input.nextShort();
-
-            switch (eleccion) {
-                case 1:
-                    System.out.println("Producto a vender: ");
-                    //////Producto.ventaProducto(3);
-                    break;
-                case 2:
-                    // retirada de dinero
-                    System.out.println("\n¿Qué cantidad?");
-                    double fondosRetirada = input.nextDouble();
-                    //////cuentaDeAlguien.retirarFondos(fondosRetirada);
-                    break;
-
-
-                case 3:
-                    // ingresar de dinero
-                    System.out.println("\n¿Qué cantidad desea ingresar?");
-                    double fondosIngreso = input.nextDouble();
-                    //////cuentaDeAlguien.ingregresarFondos(fondosIngreso);
-                    break;
-                case 4:
-                    System.out.println("indique el nombre del nuevo titular de la cuenta.");
-                    String nuevoTitular = input.next();
-                    //////cuentaDeAlguien.nombreTitular = nuevoTitular;
-                    break;
-                default:
-                    System.out.println("Muchas gracias y hasta pronto");
-
-            }
-
-        } while (eleccion == 1 || eleccion == 2 || eleccion ==3 || eleccion ==4);
-
+        Vendedor vendedor = new Vendedor(nombreVendedor);
+        Producto producto = new Producto(nombreProducto, precioVenta, precioCoste, vendedor);
+        vendedor.venderProducto(producto);
+        tienda.agregarProducto(producto);
     }
-*/
-
-
-
 }
-
 
